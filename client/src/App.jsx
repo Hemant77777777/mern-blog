b7af60548b7c4be9eb107a45a7560770574d7a46
Hemant7777777 <hemantraju7777777@gmail.com>
2024-12-26 00:24:47 +0530

    created the full footer section and app.jsx modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 // import PrivateRoute from './components/PrivateRoute';
 // import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 // // import CreatePost from './pages/CreatePost';
@@ -34,6 +34,7 @@ export default function App() {
         <Route path='/projects' element={<Projects />} />
        
       </Routes>
+      <Footer/>
       
     </BrowserRouter>
   );
