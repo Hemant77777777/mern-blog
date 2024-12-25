2794f8d788758231fd0c423b235e9c15fdd3f954
Hemant7777777 <hemantraju7777777@gmail.com>
2024-12-25 17:45:45 +0530

    created the basics of NAVBAR

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,19 +5,36 @@ import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
+// import Footer from './components/Footer';
+// import PrivateRoute from './components/PrivateRoute';
+// import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+// // import CreatePost from './pages/CreatePost';
+// import UpdatePost from './pages/UpdatePost';
+// import PostPage from './pages/PostPage';
+// import ScrollToTop from './components/ScrollToTop';
+// import Search from './pages/Search';
+
 export default function App() {
   return (
-   <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Home />} />
+    <BrowserRouter>
+     
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/projects' element={<Projects/>}/>
+      
+       
+          <Route path='/dashboard' element={<Dashboard />} />
+       
+       
 
-    </Routes>
-   </BrowserRouter>
-  )
+        <Route path='/projects' element={<Projects />} />
+       
+      </Routes>
+      
+    </BrowserRouter>
+  );
 }
-
