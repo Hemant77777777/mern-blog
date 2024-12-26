afadfa8db203ff8fe561ec497108ed2c61d7072e
Hemant7777777 <hemantraju7777777@gmail.com>
2024-12-26 20:40:10 +0530

    always goes to top of page  function created

--- client/src/components/ScrollToTop.jsx
+++ client/src/components/ScrollToTop.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-export default function ScrollToTop() {
-  return (
-    <div>ScrollToTop</div>
-  )
-}
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+export default ScrollToTop;
